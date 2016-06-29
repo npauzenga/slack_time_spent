b9d685e3a28599e2185b10f28b2768ad5d0cdff3
Nate <npauzenga@gmail.com>
2016-06-28 20:52:55 -0400

    refine formatting

--- commands/slacktrack.rb
+++ commands/slacktrack.rb
@@ -5,37 +5,43 @@ class SlackTrack < SlackRubyBot::Commands::Base
     @time_spent = 0
   end
 
-  def format_time(seconds)
+  def self.format_time(seconds)
     minutes = 0
 
     while seconds >= 60
-      minutes++
+      minutes += 1
       seconds -= 60
     end
 
-    return "#{minutes} minutes, and #{seconds} seconds"
+    return "#{minutes} minutes, and #{seconds.round} seconds"
   end
 
+  # run with "punch in". If timespent bot is present, it will set your time_in and
+  # respond with a confirmation
   match /^punch in$/ do |client, data, match|
     @time_in = Time.now
     client.say(channel: data.channel, text: "You've punched in")
   end
 
+  # run with "punch out". If timespent bot is present, it will respond with
+  # your total time logged in this session an reset your time_in
   match /^punch out$/ do |client, data, match|
     @time_out = Time.now
     @time_spent = @time_out - @time_in
     @time_in = 0
 
     client.say(channel: data.channel,
-               text: "You punched out at #{@time_out}. You logged #{@time_spent.minute}")
+               text: "You punched out. You logged #{SlackTrack.format_time(@time_spent)}")
   end
 
+  # run with "slacktrack", If timespent bot is present, it will respond with
+  # your total time logged in this session
   match /^slacktrack$/ do |client, data, match|
-    if @time_in == 0
-      client.say(channel: data.channel, text: "log back in to resume tracking")
+    if @time_in == 0 || @time_in == nil
+      client.say(channel: data.channel, text: "log in to resume tracking")
     else
       @time_spent = Time.now - @time_in
-      client.say(channel: data.channel, text: (@time_spent.minute))
+      client.say(channel: data.channel, text: SlackTrack.format_time(@time_spent))
     end
   end
 end
